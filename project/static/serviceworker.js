a29f230603a8477526a7734cf3592592949369c9
Palun Martinez <palunmartinez@gmail.com>
2026-05-10 07:56:55 -0300

    aplicacion descargable

---
+++ project/static/serviceworker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'gastarin-v1.1';
+
+const urlsToCache = [
+    '/',
+    '/static/css/style.css',
+    '/static/js/header.js',
+    '/static/js/index.js',
+    '/static/img/icon_gastarin.png',
+    '/static/img/icon-192.png',
+    '/static/img/icon-512.png',
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
